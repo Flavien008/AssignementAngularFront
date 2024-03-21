23aae0d67a33b22c5b86a5bc76eedd51aa759485
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-03-21 16:49:31 +0300

    ajout rendu

---
+++ src/app/rendu/rendu-add/rendu-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rendu-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './rendu-add.component.html',
+  styleUrl: './rendu-add.component.css'
+})
+export class RenduAddComponent {
+
+}

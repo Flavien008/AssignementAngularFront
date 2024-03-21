95e8242535caabbd82dcaa59b0d7bdb0dc705741
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-03-21 16:49:46 +0300

    liste rendu

---
+++ src/app/rendu/rendu-liste/rendu-liste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rendu-liste',
+  standalone: true,
+  imports: [],
+  templateUrl: './rendu-liste.component.html',
+  styleUrl: './rendu-liste.component.css'
+})
+export class RenduListeComponent {
+
+}

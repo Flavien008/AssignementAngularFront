b96a356d202da3fc0f9e2c3296676b62b0f592a8
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-03-21 16:51:12 +0300

    scroll infini

--- src/app/assignments/assignment-detail/assignment-detail.component.ts
+++ src/app/assignments/assignment-detail/assignment-detail.component.ts
@@ -9,21 +9,38 @@ import { Assignment } from '../assignment.model';
 import { AssignmentsService } from '../../shared/assignments.service';
 import  {RouterLink} from '@angular/router';
 import { AuthService } from '../../shared/auth.service';
+import {CdkVirtualScrollViewport,ScrollingModule} from '@angular/cdk/scrolling';
+import { MatListModule } from '@angular/material/list';
+import { ViewChild, NgZone } from '@angular/core';
+import { filter, map, pairwise, tap, throttleTime } from 'rxjs';
+
 @Component({
   selector: 'app-assignment-detail',
   standalone: true,
   imports: [CommonModule, RouterLink,
-    MatButtonModule, MatCardModule, MatCheckboxModule],
+    MatButtonModule, MatCardModule, MatCheckboxModule,CdkVirtualScrollViewport,ScrollingModule,MatListModule],
   templateUrl: './assignment-detail.component.html',
   styleUrl: './assignment-detail.component.css'
 })
 export class AssignmentDetailComponent implements OnInit {
   assignmentTransmis!: Assignment|undefined;
+  page = 1;
+  limit = 10;
+  totalDocs!: number;
+  totalPages!: number;
+  nextPage!: number;
+  prevPage!: number;
+  hasNextPage!: boolean;
+  hasPrevPage!: boolean;
+
+  @ViewChild('scroller') scroller!: CdkVirtualScrollViewport;
 
   constructor(private assignmentsService:AssignmentsService,
               private authService:AuthService,
               private route:ActivatedRoute,
-              private router:Router) { }
+              private router:Router,private ngZone: NgZone) { }
+              
+  assignments: Assignment[] = [];
 
   ngOnInit() {
     // Recuperation des query params (ce qui suit le ? dans l'url)
@@ -33,11 +50,92 @@ export class AssignmentDetailComponent implements OnInit {
 
     // On recupere l'id de l'assignment dans l'URL à l'aide de ActivatedRoute
     const id = this.route.snapshot.params['id'];
+    console.log('ngOnInit assignments, appelée AVANT affichage du composant');
+    this.getAssignmentsFromService();
     // On utilise le service pour récupérer l'assignment avec cet id
     this.assignmentsService.getAssignment(id)
     .subscribe(assignment => {
       this.assignmentTransmis = assignment;
     });
+  }
+
+  getAssignmentsFromService() {
+    // on récupère les assignments depuis le service
+    this.assignmentsService
+      .getAssignmentsPagines(this.page, this.limit)
+      .subscribe((data) => {
+        // les données arrivent ici au bout d'un certain temps
+        this.assignments = data.docs;
+        console.log('Données arrivées'+this.assignments);
+        this.totalDocs = data.totalDocs;
+        this.totalPages = data.totalPages;
+        this.nextPage = data.nextPage;
+        this.prevPage = data.prevPage;
+        this.hasNextPage = data.hasNextPage;
+        this.hasPrevPage = data.hasPrevPage;
+      });
+    console.log('Requête envoyée');
+  }
+
+
+  ngAfterViewInit() {
+    console.log(' ----- after view init ----');
+
+    if (!this.scroller) return;
+
+    // on s'abonne à l'évènement scroll du virtual scroller
+    this.scroller
+      .elementScrolled()
+      .pipe(
+        tap(() => {
+          //const dist = this.scroller.measureScrollOffset('bottom');
+          /*console.log(
+            'dans le tap, distance par rapport au bas de la fenêtre = ' + dist
+          );*/
+        }),
+        map((event) => {
+          return this.scroller.measureScrollOffset('bottom');
+        }),
+        pairwise(),
+        filter(([y1, y2]) => {
+          return y2 < y1 && y2 < 100;
+        }),
+        // Pour n'envoyer des requêtes que toutes les 200ms
+        throttleTime(200)
+      )
+      .subscribe(() => {
+        // On ne rentre que si on scrolle vers le bas, que si
+        // la distance de la scrollbar est < 100 pixels et que
+        // toutes les 200 ms
+          console.log('On demande de nouveaux assignments');
+          // on va faire une requête pour demander les assignments suivants
+          // et on va concatener le resultat au tableau des assignments courants
+          console.log('je CHARGE DE NOUVELLES DONNEES page = ' + this.page);
+          this.ngZone.run(() => {
+            if (!this.hasNextPage) return;
+            console.log("next page"+this.nextPage);
+            this.page = this.nextPage;
+            this.getAssignmentsFromServicePourScrollInfini();
+          });
+      });
+  }
+
+  getAssignmentsFromServicePourScrollInfini() {
+    // on récupère les assignments depuis le service
+    this.assignmentsService
+      .getAssignmentsPagines(this.page, this.limit)
+      .subscribe((data) => {
+        // les données arrivent ici au bout d'un certain temps
+        console.log('Données arrivées scroll');
+        this.assignments = [...this.assignments, ...data.docs];
+        this.totalDocs = data.totalDocs;
+        this.totalPages = data.totalPages;
+        this.nextPage = data.nextPage;
+        this.prevPage = data.prevPage;
+        this.hasNextPage = data.hasNextPage;
+        this.hasPrevPage = data.hasPrevPage;
+      });
+    console.log('Requête envoyée');
   }
 
   onAssignmentRendu() {

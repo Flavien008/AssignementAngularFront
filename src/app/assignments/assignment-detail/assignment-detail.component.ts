429238f3e0b484c0a39732e82f1ce210b3273c2c
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-03-22 15:00:34 +0300

    Liste rendu & correction upload

--- src/app/assignments/assignment-detail/assignment-detail.component.ts
+++ src/app/assignments/assignment-detail/assignment-detail.component.ts
@@ -16,12 +16,15 @@ import { filter, map, pairwise, tap, throttleTime } from 'rxjs';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-assignment-detail', 
   standalone: true,
   imports: [CommonModule, RouterLink,
-    MatButtonModule, MatCardModule, MatCheckboxModule,CdkVirtualScrollViewport,ScrollingModule,MatListModule,MatSelectModule,MatInputModule,MatFormFieldModule],
+    MatButtonModule, MatCardModule, MatCheckboxModule,CdkVirtualScrollViewport,ScrollingModule,MatListModule,MatSelectModule,MatInputModule,MatFormFieldModule,MatIconModule],
   templateUrl: './assignment-detail.component.html',
   styleUrl: './assignment-detail.component.css'
 })
---
+++ src/app/assignments/liste-rendu/liste-rendu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-rendu',
+  standalone: true,
+  imports: [],
+  templateUrl: './liste-rendu.component.html',
+  styleUrl: './liste-rendu.component.css'
+})
+export class ListeRenduComponent {
+
+}

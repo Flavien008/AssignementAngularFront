7f8fe1c2a51ea54db1d907cc37cf25e5b1d6b5d4
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-09 14:05:01 +0300

    Creation du modele rendu

---
+++ src/app/assignments/rendu.model.ts
@@ -0,0 +1,13 @@
+export class Rendu {
+    _id!: string;
+    idEtudiant!: string;
+    idAssignment!: string;
+    matricule!: string;
+    auteur!: string;
+    dateRendu!: string;
+    file!: string;
+    description!: string;
+    note!: string;
+    remarque!: string;
+  }
+  

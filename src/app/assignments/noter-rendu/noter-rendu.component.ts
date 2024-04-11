5162fc4b5aaebecf7e9f9450ef2d7ea2c6dfa9df
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-11 17:23:16 +0300

    Components noter rendu

---
+++ src/app/assignments/noter-rendu/noter-rendu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noter-rendu',
+  standalone: true,
+  imports: [],
+  templateUrl: './noter-rendu.component.html',
+  styleUrl: './noter-rendu.component.css'
+})
+export class NoterRenduComponent {
+
+}

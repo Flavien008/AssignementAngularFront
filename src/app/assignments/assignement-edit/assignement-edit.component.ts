00685ad65ebd38118f959e7f872bc51c5f5d851d
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-04-11 19:10:18 +0300

    close dialog when modifi is finish

--- src/app/assignments/assignement-edit/assignement-edit.component.ts
+++ src/app/assignments/assignement-edit/assignement-edit.component.ts
@@ -3,54 +3,53 @@ import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import {
-  MatDialogClose,
-  MatDialogActions,
-  MAT_DIALOG_DATA,
-  MatDialogTitle,
-  MatDialogContent,
-  MatDialogRef,
+    MatDialogClose,
+    MatDialogActions,
+    MAT_DIALOG_DATA,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogRef,
 } from '@angular/material/dialog';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-assignement-edit',
-  standalone: true,
-  imports: [MatInput,ReactiveFormsModule,MatFormField,MatButton,MatDialogActions,MatDialogTitle, MatDialogContent,MatDialogClose],
-  templateUrl: './assignement-edit.component.html',
-  styleUrl: './assignement-edit.component.css'
+    selector: 'app-assignement-edit',
+    standalone: true,
+    imports: [MatInput, ReactiveFormsModule, MatFormField, MatButton, MatDialogActions, MatDialogTitle, MatDialogContent, MatDialogClose],
+    templateUrl: './assignement-edit.component.html',
+    styleUrl: './assignement-edit.component.css'
 })
 
 export class AssignementEditComponent {
-  assignmentForm: FormGroup;
-  datePipe = new DatePipe('en-US');
-  constructor(private dialogRef: MatDialogRef<AssignementEditComponent>,@Inject(MAT_DIALOG_DATA) public data: any,
-  private formBuilder: FormBuilder,private assignmentsService:AssignmentsService)
-  {
-    const dateLimiteValue = data.assignement?.dateLimite ? this.datePipe.transform(new Date(data.assignement?.dateLimite), 'yyyy-MM-dd') : '';
-    this.assignmentForm = this.formBuilder.group({
-      _id : [data.assignement._id],
-      titre: [data.assignement?.titre || ''],
-      description: [data.assignement?.description || ''],
-      dateLimite: [ dateLimiteValue || ''],
-      matiere: [data.assignement?.matiere || ''],
-      lien: [data.assignement?.lien || '']
-    });
-  }
+    assignmentForm: FormGroup;
+    datePipe = new DatePipe('en-US');
+    constructor(private dialogRef: MatDialogRef<AssignementEditComponent>, @Inject(MAT_DIALOG_DATA) public data: any,
+        private formBuilder: FormBuilder, private assignmentsService: AssignmentsService) {
+        const dateLimiteValue = data.assignement?.dateLimite ? this.datePipe.transform(new Date(data.assignement?.dateLimite), 'yyyy-MM-dd') : '';
+        this.assignmentForm = this.formBuilder.group({
+            _id: [data.assignement._id],
+            titre: [data.assignement?.titre || ''],
+            description: [data.assignement?.description || ''],
+            dateLimite: [dateLimiteValue || ''],
+            matiere: [data.assignement?.matiere || ''],
+            lien: [data.assignement?.lien || '']
+        });
+    }
 
-  saveChanges() {
-    const modifiedAssignment = this.assignmentForm.value;
-    this.assignmentsService
-        .updateAssignment(modifiedAssignment)
-        .subscribe((message) => {
-        console.log(message);
-      });
-    this.dialogRef.close(true);
-  }
+    saveChanges() {
+        const modifiedAssignment = this.assignmentForm.value;
+        this.assignmentsService
+            .updateAssignment(modifiedAssignment)
+            .subscribe((message) => {
+                console.log(message);
+                this.dialogRef.close(true);
+            });
+    }
 
-  closeDialog() {
-    this.dialogRef.close(false);
-  }
+    closeDialog() {
+        this.dialogRef.close(false);
+    }
 }
--- src/app/assignments/assignment-list/assignment-list.component.ts
+++ src/app/assignments/assignment-list/assignment-list.component.ts
@@ -8,216 +8,216 @@ import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { Assignment } from '../assignment.model';
 import { AssignmentsService } from '../../shared/assignments.service';
-import  {RouterLink} from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { AuthService } from '../../shared/auth.service';
-import {CdkVirtualScrollViewport,ScrollingModule} from '@angular/cdk/scrolling';
+import { CdkVirtualScrollViewport, ScrollingModule } from '@angular/cdk/scrolling';
 import { MatListModule } from '@angular/material/list';
 import { ViewChild, NgZone } from '@angular/core';
 import { filter, map, pairwise, tap, throttleTime } from 'rxjs';
 import { MatFormField } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatOption } from '@angular/material/core';
-import {MatDialog, MatDialogClose} from '@angular/material/dialog';
+import { MatDialog, MatDialogClose } from '@angular/material/dialog';
 import { AssignementEditComponent } from '../assignement-edit/assignement-edit.component';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 @Component({
-  selector: 'app-assignment-list', 
-  standalone: true,
-  imports: [MatSpinner,MatDialogClose,MatOption,MatInputModule,MatFormFieldModule,FormsModule,MatIcon,MatFormField,CommonModule, RouterLink,
-    MatButtonModule, MatCardModule, MatCheckboxModule,CdkVirtualScrollViewport,ScrollingModule,MatListModule],
-  templateUrl: './assignment-list.component.html',
-  styleUrl: './assignment-list.component.css'
+    selector: 'app-assignment-list',
+    standalone: true,
+    imports: [MatSpinner, MatDialogClose, MatOption, MatInputModule, MatFormFieldModule, FormsModule, MatIcon, MatFormField, CommonModule, RouterLink,
+        MatButtonModule, MatCardModule, MatCheckboxModule, CdkVirtualScrollViewport, ScrollingModule, MatListModule],
+    templateUrl: './assignment-list.component.html',
+    styleUrl: './assignment-list.component.css'
 })
 export class AssignmentListComponent implements OnInit {
-  assignmentTransmis!: Assignment|undefined;
-  titrefiltre = '';
-  matierefiltre = '';
-  groupeid = '';
-  page = 1;
-  limit = 10;
-  totalDocs!: number;
-  totalPages!: number;
-  nextPage!: number;
-  prevPage!: number;
-  hasNextPage!: boolean;
-  hasPrevPage!: boolean;
-  isLoadingAssignments: boolean = false;
+    assignmentTransmis!: Assignment | undefined;
+    titrefiltre = '';
+    matierefiltre = '';
+    groupeid = '';
+    page = 1;
+    limit = 10;
+    totalDocs!: number;
+    totalPages!: number;
+    nextPage!: number;
+    prevPage!: number;
+    hasNextPage!: boolean;
+    hasPrevPage!: boolean;
+    isLoadingAssignments: boolean = false;
 
-  @ViewChild('scroller') scroller!: CdkVirtualScrollViewport;
+    @ViewChild('scroller') scroller!: CdkVirtualScrollViewport;
 
-  constructor(private assignmentsService:AssignmentsService,
-              private authService:AuthService,
-              private route:ActivatedRoute,
-              private router:Router,private ngZone: NgZone,
-              public dialog: MatDialog) { }
+    constructor(private assignmentsService: AssignmentsService,
+        private authService: AuthService,
+        private route: ActivatedRoute,
+        private router: Router, private ngZone: NgZone,
+        public dialog: MatDialog) { }
 
-    openDialog(assignement:Assignment) {
-        const dialogEditref =this.dialog.open(AssignementEditComponent, {
+    openDialog(assignement: Assignment) {
+        const dialogEditref = this.dialog.open(AssignementEditComponent, {
             data: { assignement }
         });
 
         dialogEditref.afterClosed().subscribe(result => {
-          console.log("confirmation : "+result);
-          if (result) { 
-              this.getAssignmentsFromService();
-          }
+            console.log("confirmation : " + result);
+            if (result) {
+                this.getAssignmentsFromService();
+            }
         });
     }
-              
-  assignments: Assignment[] = [];
-  
 
-  ngOnInit() {
-    // Recuperation des query params (ce qui suit le ? dans l'url)
-    console.log(this.route.snapshot.queryParams);
-    // Recuperation des fragment (ce qui suit le # dans l'url)
-    console.log(this.route.snapshot.fragment);
+    assignments: Assignment[] = [];
 
-    // On recupere l'id de l'assignment dans l'URL à l'aide de ActivatedRoute
-    const id = this.route.snapshot.params['id'];
-    this.groupeid = id ? id : '';
-    this.getAssignmentsFromService();
-    // On utilise le service pour récupérer l'assignment avec cet id
-    this.assignmentsService.getAssignment(id)
-    .subscribe(assignment => {
-      this.assignmentTransmis = assignment;
-    });
-  }
+
+    ngOnInit() {
+        // Recuperation des query params (ce qui suit le ? dans l'url)
+        console.log(this.route.snapshot.queryParams);
+        // Recuperation des fragment (ce qui suit le # dans l'url)
+        console.log(this.route.snapshot.fragment);
+
+        // On recupere l'id de l'assignment dans l'URL à l'aide de ActivatedRoute
+        const id = this.route.snapshot.params['id'];
+        this.groupeid = id ? id : '';
+        this.getAssignmentsFromService();
+        // On utilise le service pour récupérer l'assignment avec cet id
+        this.assignmentsService.getAssignment(id)
+            .subscribe(assignment => {
+                this.assignmentTransmis = assignment;
+            });
+    }
 
     applyFilters(): void {
         this.getAssignmentsFromService();
     }
 
-  getAssignmentsFromService() {
-    // on récupère les assignments depuis le service
-    this.isLoadingAssignments = true;
-    this.assignmentsService
-      .getAssignmentsPaginesListe(this.page, this.limit,this.titrefiltre,this.matierefiltre,this.groupeid)
-      .subscribe((data) => {
-        // les données arrivent ici au bout d'un certain temps
-        this.assignments = data.docs;
-        console.log('Données arrivées'+this.assignments);
-        this.totalDocs = data.totalDocs;
-        this.totalPages = data.totalPages;
-        this.nextPage = data.nextPage;
-        this.prevPage = data.prevPage;
-        this.hasNextPage = data.hasNextPage;
-        this.hasPrevPage = data.hasPrevPage;
-        this.isLoadingAssignments = false;
-      });
-    console.log('Requête envoyée');
-  }
-
-
-  ngAfterViewInit() {
-    console.log(' ----- after view init ----');
-
-    if (!this.scroller) return;
-
-    // on s'abonne à l'évènement scroll du virtual scroller
-    this.scroller
-      .elementScrolled()
-      .pipe(
-        tap(() => {
-          //const dist = this.scroller.measureScrollOffset('bottom');
-          /*console.log(
-            'dans le tap, distance par rapport au bas de la fenêtre = ' + dist
-          );*/
-        }),
-        map((event) => {
-          return this.scroller.measureScrollOffset('bottom');
-        }),
-        pairwise(),
-        filter(([y1, y2]) => {
-          return y2 < y1 && y2 < 100;
-        }),
-        // Pour n'envoyer des requêtes que toutes les 200ms
-        throttleTime(200)
-      )
-      .subscribe(() => {
-        // On ne rentre que si on scrolle vers le bas, que si
-        // la distance de la scrollbar est < 100 pixels et que
-        // toutes les 200 ms
-          console.log('On demande de nouveaux assignments');
-          // on va faire une requête pour demander les assignments suivants
-          // et on va concatener le resultat au tableau des assignments courants
-          console.log('je CHARGE DE NOUVELLES DONNEES page = ' + this.page);
-          this.ngZone.run(() => {
-            if (!this.hasNextPage) return;
-            console.log("next page"+this.nextPage);
-            this.page = this.nextPage;
-            this.getAssignmentsFromServicePourScrollInfini();
-          });
-      });
-  }
-
-  getAssignmentsFromServicePourScrollInfini() {
-    // on récupère les assignments depuis le service
-    this.assignmentsService
-      .getAssignmentsPaginesListe(this.page, this.limit,this.titrefiltre,this.matierefiltre,this.groupeid)
-      .subscribe((data) => {
-        // les données arrivent ici au bout d'un certain temps
-        console.log('Données arrivées scroll');
-        this.assignments = [...this.assignments, ...data.docs];
-        this.totalDocs = data.totalDocs;
-        this.totalPages = data.totalPages;
-        this.nextPage = data.nextPage;
-        this.prevPage = data.prevPage;
-        this.hasNextPage = data.hasNextPage;
-        this.hasPrevPage = data.hasPrevPage;
-      });
-    console.log('Requête envoyée');
-  }
-
-  onAssignmentRendu() {
-    // on a cliqué sur la checkbox, on change le statut de l'assignment
-    if(this.assignmentTransmis) {
-      // this.assignmentTransmis.rendu = true;
-      this.assignmentsService.updateAssignment(this.assignmentTransmis)
-      .subscribe(message => {
-        console.log(message);
-        // on navigue vers la liste des assignments
-        this.router.navigate(['/home']);
-      });
+    getAssignmentsFromService() {
+        // on récupère les assignments depuis le service
+        this.isLoadingAssignments = true;
+        this.assignmentsService
+            .getAssignmentsPaginesListe(this.page, this.limit, this.titrefiltre, this.matierefiltre, this.groupeid)
+            .subscribe((data) => {
+                // les données arrivent ici au bout d'un certain temps
+                this.assignments = data.docs;
+                console.log('Données arrivées' + this.assignments);
+                this.totalDocs = data.totalDocs;
+                this.totalPages = data.totalPages;
+                this.nextPage = data.nextPage;
+                this.prevPage = data.prevPage;
+                this.hasNextPage = data.hasNextPage;
+                this.hasPrevPage = data.hasPrevPage;
+                this.isLoadingAssignments = false;
+            });
+        console.log('Requête envoyée');
     }
-  }
 
-  onDelete() {
-    // on va directement utiliser le service
-    if(this.assignmentTransmis) {
-      this.assignmentsService.deleteAssignment(this.assignmentTransmis)
-      .subscribe(message => {
-        console.log(message);
-        // on va cacher la vue de detail en mettant assignmentTransmis à undefined
-        this.assignmentTransmis = undefined;
-        // on navigue vers la liste des assignments
-        this.router.navigate(['/home']);
-      });
+
+    ngAfterViewInit() {
+        console.log(' ----- after view init ----');
+
+        if (!this.scroller) return;
+
+        // on s'abonne à l'évènement scroll du virtual scroller
+        this.scroller
+            .elementScrolled()
+            .pipe(
+                tap(() => {
+                    //const dist = this.scroller.measureScrollOffset('bottom');
+                    /*console.log(
+                      'dans le tap, distance par rapport au bas de la fenêtre = ' + dist
+                    );*/
+                }),
+                map((event) => {
+                    return this.scroller.measureScrollOffset('bottom');
+                }),
+                pairwise(),
+                filter(([y1, y2]) => {
+                    return y2 < y1 && y2 < 100;
+                }),
+                // Pour n'envoyer des requêtes que toutes les 200ms
+                throttleTime(200)
+            )
+            .subscribe(() => {
+                // On ne rentre que si on scrolle vers le bas, que si
+                // la distance de la scrollbar est < 100 pixels et que
+                // toutes les 200 ms
+                console.log('On demande de nouveaux assignments');
+                // on va faire une requête pour demander les assignments suivants
+                // et on va concatener le resultat au tableau des assignments courants
+                console.log('je CHARGE DE NOUVELLES DONNEES page = ' + this.page);
+                this.ngZone.run(() => {
+                    if (!this.hasNextPage) return;
+                    console.log("next page" + this.nextPage);
+                    this.page = this.nextPage;
+                    this.getAssignmentsFromServicePourScrollInfini();
+                });
+            });
     }
-  }
 
-  confirmDelete(assignment: any): void {
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      width: '500px',
-      data: { assignment: assignment }
-    });
+    getAssignmentsFromServicePourScrollInfini() {
+        // on récupère les assignments depuis le service
+        this.assignmentsService
+            .getAssignmentsPaginesListe(this.page, this.limit, this.titrefiltre, this.matierefiltre, this.groupeid)
+            .subscribe((data) => {
+                // les données arrivent ici au bout d'un certain temps
+                console.log('Données arrivées scroll');
+                this.assignments = [...this.assignments, ...data.docs];
+                this.totalDocs = data.totalDocs;
+                this.totalPages = data.totalPages;
+                this.nextPage = data.nextPage;
+                this.prevPage = data.prevPage;
+                this.hasNextPage = data.hasNextPage;
+                this.hasPrevPage = data.hasPrevPage;
+            });
+        console.log('Requête envoyée');
+    }
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result && result.confirmed) { // Vérifiez si la confirmation est vraie
-          console.log(result.assignment); // Ceci vous donne accès à l'assignment sélectionné
-          this.assignmentsService.deleteAssignment(result.assignment).subscribe(message => {
-          console.log(message);
-          this.getAssignmentsFromService();
-      });
-      }
-    });
-  }
+    onAssignmentRendu() {
+        // on a cliqué sur la checkbox, on change le statut de l'assignment
+        if (this.assignmentTransmis) {
+            // this.assignmentTransmis.rendu = true;
+            this.assignmentsService.updateAssignment(this.assignmentTransmis)
+                .subscribe(message => {
+                    console.log(message);
+                    // on navigue vers la liste des assignments
+                    this.router.navigate(['/home']);
+                });
+        }
+    }
 
-  isAdmin() {
-    return this.authService.loggedIn;
-  }
+    onDelete() {
+        // on va directement utiliser le service
+        if (this.assignmentTransmis) {
+            this.assignmentsService.deleteAssignment(this.assignmentTransmis)
+                .subscribe(message => {
+                    console.log(message);
+                    // on va cacher la vue de detail en mettant assignmentTransmis à undefined
+                    this.assignmentTransmis = undefined;
+                    // on navigue vers la liste des assignments
+                    this.router.navigate(['/home']);
+                });
+        }
+    }
+
+    confirmDelete(assignment: any): void {
+        const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+            width: '500px',
+            data: { assignment: assignment }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result && result.confirmed) { // Vérifiez si la confirmation est vraie
+                console.log(result.assignment); // Ceci vous donne accès à l'assignment sélectionné
+                this.assignmentsService.deleteAssignment(result.assignment).subscribe(message => {
+                    console.log(message);
+                    this.getAssignmentsFromService();
+                });
+            }
+        });
+    }
+
+    isAdmin() {
+        return this.authService.loggedIn;
+    }
 }

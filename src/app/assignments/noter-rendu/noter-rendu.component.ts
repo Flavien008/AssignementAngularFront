eff6d2b29f741e1769955279215a22ff06597820
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-12 16:33:50 +0300

    tabulation code assignment list

--- src/app/assignments/noter-rendu/noter-rendu.component.ts
+++ src/app/assignments/noter-rendu/noter-rendu.component.ts
@@ -5,10 +5,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { AssignmentDetailComponent } from '../assignment-detail/assignment-detail.component';
 
 @Component({
   selector: 'app-noter-rendu',
@@ -26,8 +27,9 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class NoterRenduComponent {
   @Input() rendu: Rendu | undefined;
+  newnote : undefined;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(private dialogRef: MatDialogRef<AssignmentDetailComponent>,@Inject(MAT_DIALOG_DATA) public data: any) {
     this.rendu = data.rendu;
     console.log(this.rendu);
   }
@@ -42,6 +44,9 @@ export class NoterRenduComponent {
         document.execCommand('copy');
         window.getSelection()?.removeAllRanges();
     }
+}
+closeDialog() {
+  this.dialogRef.close(false);
 }
 
 

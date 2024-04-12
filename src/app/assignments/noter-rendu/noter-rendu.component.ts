8be37c19d2f676d67689dcc83928f4d5cae6a405
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-12 16:01:44 +0300

    Noter et remarque rendu

--- src/app/assignments/noter-rendu/noter-rendu.component.ts
+++ src/app/assignments/noter-rendu/noter-rendu.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { Rendu } from '../rendu.model';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-noter-rendu',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatCardModule, MatDividerModule, MatButtonModule, MatIconModule
+  ],
   templateUrl: './noter-rendu.component.html',
   styleUrl: './noter-rendu.component.css'
 })
 export class NoterRenduComponent {
+  @Input() rendu: Rendu | undefined;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    this.rendu = data.rendu;
+    console.log(this.rendu);
+  }
+
+  copyLinkToClipboard() {
+    const linkElement = document.getElementById('gitLink');
+    if (linkElement) {
+        const range = document.createRange();
+        range.selectNode(linkElement);
+        window.getSelection()?.removeAllRanges();
+        window.getSelection()?.addRange(range);
+        document.execCommand('copy');
+        window.getSelection()?.removeAllRanges();
+        alert('Lien copié dans le presse-papiers');
+    }
+}
+
 
 }

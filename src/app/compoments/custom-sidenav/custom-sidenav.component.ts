a6599b21d8bb473a2d42a2fbdfcbef0da9aa8b1e
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-17 21:27:41 +0300

    Matiere on sidenav

--- src/app/compoments/custom-sidenav/custom-sidenav.component.ts
+++ src/app/compoments/custom-sidenav/custom-sidenav.component.ts
@@ -10,50 +10,55 @@ import { User } from '../../login/user.model';
 export type MenuItem = {
     icon: string;
     label: string;
-    route?:string;
+    route?: string;
 }
 
 @Component({
-  selector: 'app-custom-sidenav',
-  standalone: true,
-  imports: [CommonModule,MatListModule,MatIconModule,RouterLink,RouterLinkActive],
-  templateUrl: './custom-sidenav.component.html',
-  styleUrl: './custom-sidenav.component.css'
+    selector: 'app-custom-sidenav',
+    standalone: true,
+    imports: [CommonModule, MatListModule, MatIconModule, RouterLink, RouterLinkActive],
+    templateUrl: './custom-sidenav.component.html',
+    styleUrl: './custom-sidenav.component.css'
 })
 export class CustomSidenavComponent {
 
-    userData : User | undefined;
+    userData: User | undefined;
 
 
     ngOnInit(): void {
         this.userData = this.getUserData();
-      }
+    }
 
     sideNavCollapsed = signal(false);
-    @Input() set collapsed(val:boolean){
+    @Input() set collapsed(val: boolean) {
         this.sideNavCollapsed.set(val);
     }
     MenuItems = signal<MenuItem[]>([
         {
-            icon : 'dashboard',
-            label : 'Dashboard',
-            route : 'dashboard',
+            icon: 'dashboard',
+            label: 'Dashboard',
+            route: 'dashboard',
         },
         {
-            icon : 'work',
-            label : 'Assignement',
-            route : 'assignments',
+            icon: 'work',
+            label: 'Assignement',
+            route: 'assignments',
         }, {
-            icon : 'groups',
-            label : 'Groups',
-            route : 'groups',
+            icon: 'groups',
+            label: 'Groups',
+            route: 'groups',
+        },
+        {
+            icon: 'book',
+            label: 'Matiere',
+            route: '',
         }
     ]);
 
-    profilePicSize = computed(() => this.sideNavCollapsed() ? '20':'100');
+    profilePicSize = computed(() => this.sideNavCollapsed() ? '20' : '100');
 
     getUserData(): any {
         const userData = localStorage.getItem('user');
         return userData ? JSON.parse(userData) : null;
-      }
+    }
 }

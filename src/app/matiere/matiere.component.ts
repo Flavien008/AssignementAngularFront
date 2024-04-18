717d384161fe3c789a12cf07be9ac536c2ca8b18
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-18 10:45:18 +0300

    titre + bouton ajout + champ recherche matiere

--- src/app/matiere/matiere.component.ts
+++ src/app/matiere/matiere.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardModule } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatList, MatListItem } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-matiere',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatIconModule, MatFormFieldModule, MatButtonModule, FormsModule, MatLabel, MatFormField, MatPaginatorModule, RouterLink, MatCard, MatCardContent, MatCardModule, MatGridListModule, MatIcon, CommonModule, MatDivider, MatListItem, MatList, MatProgressSpinnerModule],
   templateUrl: './matiere.component.html',
   styleUrl: './matiere.component.css'
 })
 export class MatiereComponent {
+  searchTerm = '';
+
+  constructor(private authService: AuthService, private dialog: MatDialog) { }
+
+  onSearchTermChange() {
+
+}
+
+isProf() {
+  return this.authService.isProf();
+}
+
+
 
 }

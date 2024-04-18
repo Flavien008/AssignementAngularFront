f2fe6c247c20da66ae5612a8fd61fee67cfe2956
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-18 22:32:19 +0300

    Drag and drop image upload

--- src/app/matiere/addmatiereDialog/add-matiere-dialog.component.ts
+++ src/app/matiere/addmatiereDialog/add-matiere-dialog.component.ts
@@ -10,6 +10,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSpinner } from '@angular/material/progress-spinner';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 
 import { Router } from '@angular/router';
 
@@ -17,23 +18,54 @@ import { Router } from '@angular/router';
     selector: 'app-add-group-dialog',
     templateUrl: './add-matiere-dialog.component.html',
     standalone: true,
-    imports: [MatSpinner, MatPaginator, MatFormFieldModule, MatInputModule, MatFormField, MatLabel, MatButton, CommonModule, FormsModule, MatDialogContent, MatDialogActions, MatCheckbox],
+    imports: [MatSpinner, MatPaginator, MatFormFieldModule, MatInputModule, MatFormField, MatLabel, MatButton, CommonModule, FormsModule, MatDialogContent, MatDialogActions, MatCheckbox, NgxDropzoneModule],
     styleUrls: ['./add-matiere-dialog.component.css']
 })
 export class AddMatiereDialogComponent implements OnInit {
     nom = '';
     addingGroupe: boolean = false;
+    files: File[] = [];
+    loading = false;
+    error = false;
+    base64textString: string = "";
 
     constructor(
         public dialogRef: MatDialogRef<AddMatiereDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any,
-        private router:Router) {
+        private router: Router) {
     }
     ngOnInit(): void {
-        
+
     }
 
     addMatiere() {
     }
+
+    onSelect(event:any) {
+		console.log(event);
+		this.files.push(...event.addedFiles);
+	}
+
+  onFileChange(event: any) {
+    const files = event.addedFiles;
+    console.log("miov a eto");
+    if (files.length > 0) {
+        const file = files[0];
+        const reader = new FileReader();
+        reader.onload = this._handleReaderLoaded.bind(this);
+        reader.readAsDataURL(file);
+    }
+}
+  _handleReaderLoaded(e : any) {
+    const reader = e.target;
+    this.base64textString = reader.result;
+    console.log(this.base64textString);
+  }
+
+	onRemove(event : any) {
+		console.log(event);
+		this.files.splice(this.files.indexOf(event), 1);
+	}
+
 }
 

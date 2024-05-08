1aa43d0fa079d8f7f61fc1eb668228f79338e199
Tendry Rakotoarivony <80846844+TendryArivony@users.noreply.github.com>
2024-05-08 15:14:49 +0300

    Envoie de mail lorsque on ajoute membre dans groupe

---
+++ src/app/shared/mail.service.ts
@@ -0,0 +1,25 @@
+
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MailService {
+    uri = environment.baseUrl + "/sendmail";
+    headers: any;
+    constructor(private http: HttpClient, private auth: AuthService) {
+        this.headers = this.auth.createAuthorizationHeader();
+    }
+
+    sendMail(data: any){
+        this.headers = this.auth.createAuthorizationHeader();
+        return this.http.post(this.uri, data, { headers: this.headers });
+    }
+
+
+}

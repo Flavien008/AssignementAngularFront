89c528295e8d8831df3e41d391e49bdc6b216cfd
Tendry Rakotoarivony <tendry-hery-ny-aina.rakotoarivony@etu.univ-cotedazur.fr>
2024-05-31 16:36:41 +0300

    Liste des rendus en tableau

--- src/app/assignments/liste-rendu/liste-rendu.component.ts
+++ src/app/assignments/liste-rendu/liste-rendu.component.ts
@@ -1,19 +1,18 @@
-import { Component, NgZone } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTable, MatTableModule } from '@angular/material/table';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Rendu } from '../rendu.model';
 import { AssignmentsService } from '../../shared/assignments.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../../shared/auth.service';
-import { CommonModule } from '@angular/common'; 
-import {MatDividerModule} from '@angular/material/divider';
-import {MatIconModule} from '@angular/material/icon';
-import {DatePipe} from '@angular/common';
-
+import { CommonModule, DatePipe } from '@angular/common'; 
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { NoterRenduComponent } from '../noter-rendu/noter-rendu.component';
 
 @Component({
   selector: 'app-liste-rendu',
@@ -21,7 +20,6 @@ import {DatePipe} from '@angular/common';
   imports: [
     RouterLink,
     MatButtonModule,
-    MatTable,
     MatTableModule,
     MatPaginatorModule,
     MatListModule,
@@ -32,10 +30,12 @@ import {DatePipe} from '@angular/common';
     DatePipe
   ],
   templateUrl: './liste-rendu.component.html',
-  styleUrl: './liste-rendu.component.css'
+  styleUrls: ['./liste-rendu.component.css']
 })
-export class ListeRenduComponent {
+export class ListeRenduComponent implements OnInit {
   rendus: Rendu[] = [];
+  dataSource = new MatTableDataSource<Rendu>(this.rendus);
+  displayedColumns: string[] = ['matierePhoto', 'nomMatiere', 'titreAssignment', 'dateRendu', 'description','details'];
   page = 1;
   limit = 9;
   totalDocs!: number;
@@ -49,24 +49,20 @@ export class ListeRenduComponent {
   filtre: string = 'all';
   chargement: boolean = false;
 
-  constructor(private assignmentsService: AssignmentsService,
+  constructor(
+    private assignmentsService: AssignmentsService,
     private authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router, private ngZone: NgZone,
+    private router: Router,
+    private ngZone: NgZone,
     public dialog: MatDialog
-  ) { }
-
-
-
-
+  ) {}
 
   ngOnInit(): void {
     this.userData = this.getUserData();
     if (this.isStudent()) {
-      this.getAllRenduFromServiceByStudent()
+      this.getAllRenduFromServiceByStudent();
     }
-
-
   }
 
   getUserData(): any {
@@ -78,36 +74,33 @@ export class ListeRenduComponent {
     this.getAllRenduFromServiceByStudent();
   }
 
-
-
-
   getAllRenduFromServiceByStudent() {
     const url = this.route.snapshot.url;
     const lastSegment = url[url.length - 1];
     const id = lastSegment.path;
-    this.chargement = true
+    this.chargement = true;
     this.assignmentsService
       .getAllRenduPaginesListeByStudent(this.page, this.limit, this.filtre, this.userData._id)
       .subscribe((data) => {
         console.log('Données arrivées rendu');
         this.rendus = data.docs;
+        this.dataSource.data = this.rendus;
         this.totalDocs = data.totalDocs;
         this.totalPages = data.totalPages;
         this.nextPage = data.nextPage;
         this.prevPage = data.prevPage;
         this.hasNextPage = data.hasNextPage;
         this.hasPrevPage = data.hasPrevPage;
-        this.chargement = false
+        this.chargement = false;
       });
     console.log('Requête envoyée');
   }
 
   onPageChange(event: PageEvent) {
-    this.page = event.pageIndex + 1; // pageIndex commence à 0, donc nous ajoutons 
+    this.page = event.pageIndex + 1; // pageIndex commence à 0, donc nous ajoutons 1
     this.limit = event.pageSize;
-    this. getAllRenduFromServiceByStudent();
+    this.getAllRenduFromServiceByStudent();
   }
-
 
   isProf() {
     return this.authService.isProf();
@@ -115,5 +108,19 @@ export class ListeRenduComponent {
 
   isStudent() {
     return this.authService.isEtudiant();
+  }
+
+  openDialog(rendu: Rendu) {
+    console.log(rendu);
+    const dialogNoteref = this.dialog.open(NoterRenduComponent, {
+      data: { rendu }
+    });
+
+    dialogNoteref.afterClosed().subscribe(result => {
+      console.log("confirmation : " + result);
+      if (result) {
+        this.getAllRenduFromServiceByStudent();
+      }
+    });
   }
 }
